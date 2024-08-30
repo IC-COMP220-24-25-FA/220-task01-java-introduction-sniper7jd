bdc5b99d7c96b44c70056c12d28d5bffc8872302
sniper7jd <jdhingra@ithaca.edu>
2024-08-30 00:39:53 -0400

    calcArea

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -1,11 +1,18 @@
 package edu.ithaca.dragon.shapes;
 
 public class Rectangle {
+    private double length;
+    private double width;
     
 
     public Rectangle(double length, double width){
-        throw new RuntimeException("Not implemented yet");
+        if (length <= 0 || width <= 0) {
+            throw new IllegalArgumentException("Length and width must be positive values.");
+        }
+        this.length = length;
+        this.width = width;
     }
+    
 
     public double calcArea(){
        throw new RuntimeException("Not implemented yet");
