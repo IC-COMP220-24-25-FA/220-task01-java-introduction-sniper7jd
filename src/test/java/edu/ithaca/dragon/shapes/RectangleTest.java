c7c9ba7161c7e80f080a5b639e376658197fef3f
sniper7jd <jdhingra@ithaca.edu>
2024-08-30 17:40:39 -0400

    rectTest

---
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -0,0 +1,45 @@
+package edu.ithaca.dragon.shapes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class RectangleTest {
+    
+    @Test
+    public void calcAreaTest(){
+        Rectangle rect = new Rectangle(10, 20);
+        assertEquals(200.0, rect.calcArea(), 0.0001);
+    }
+        
+        // myCircle = new Circle(5.5);
+        // assertEquals(95.0331, myCircle.calcArea(), 0.0001);
+
+        // myCircle = new Circle(0.001);
+        // assertEquals(0.00000314159, myCircle.calcArea(), 0.0000000001);
+    
+
+//     @Test
+//     public void longestLineWithinTest(){
+//         Circle myCircle = new Circle(3);
+//         assertEquals(6, myCircle.longestLineWithin(), 0.001, "The longest line within the circle should be twice the radius.");
+//     }
+
+//     @Test
+//     public void doubleSizeTest() {
+//         Circle myCircle = new Circle(4);
+//         myCircle.doubleSize();
+//         assertEquals(8.0, myCircle.getRadius(), 0.001, "The radius should be doubled.");
+//     }
+
+//     @Test
+//     public void TestConstructorError(){
+//         assertThrows(IllegalArgumentException.class, () -> new Circle(0));
+//     }
+
+
+    
+// }
