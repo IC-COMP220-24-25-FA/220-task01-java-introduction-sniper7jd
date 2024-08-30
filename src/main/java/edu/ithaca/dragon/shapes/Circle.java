513faf34af6fc7b339e565f58473e18d4bd22b05
sniper7jd <jdhingra@ithaca.edu>
2024-08-30 00:37:54 -0400

    defined length and width

--- src/main/java/edu/ithaca/dragon/shapes/Circle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Circle.java
@@ -7,6 +7,9 @@ public class Circle {
      * @throws IllegalArgumentException if radius is not a positive number
      */
     public Circle(double radius){
+    if (radius <= 0){
+        throw new IllegalArgumentException("Radius must be positive");
+    }
         this.radius = radius;
     }
 
@@ -14,20 +17,20 @@ public class Circle {
      * @return the area of this circle
      */
     public double calcArea(){
-        throw new RuntimeException("Not implemented yet");
+        return this.radius * this.radius * Math.PI;
     }
 
     /**
      * @post doubles the size of this circle
      */
     public void doubleSize(){
-        throw new RuntimeException("Not implemented yet");
+        this.radius *= 2;
     }
 
     /**
      * @return the longest straight line that can be drawn within this circle
      */
     public double longestLineWithin(){
-        throw new RuntimeException("Not implemented yet");
+        return this.radius * 2;
     }
 }
